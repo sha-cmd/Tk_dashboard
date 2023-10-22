851f57aec0c28fc2cbbbd30f8a22b6d9a6ebf72d
Romain Boyrie <romain@boyrie.email>
2023-10-22 18:43:57 +0200

    First commits
    Add main program.

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.objects.view.Application import Application
+
+
+def main():
+    Application().mainloop()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == "__main__":
+    main()

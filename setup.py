7d3a2f0ddc09b3493cbf44f92b02302c5e6cf7b7
Romain Boyrie <romain@boyrie.email>
2023-10-28 17:46:33 +0200

    Setup
    Amélioration du setup.

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
+from setuptools import setup
+
 with open('README.rst', 'r') as fh:
     long_description = fh.read()
 
 setup(
-    name='Mon Programme',
+    name='Dashboard & Tk Application',
     version='1.0',
     author='Romain BOYRIE',
     author_email='romain@boyrie.email',
-    description='Financial Data Analysis',
-    url="http://github.example.com",
+    description='Data viz',
+    url="https://github.com/sha-cmd/Tk_dashboard",
     license='GNU V3 License',
     long_description=long_description,
-    packages=['mon_programme'],
-    install_requires=['psycopg3', 'ttkbootstrap', 'matplotlib'],
+    packages=['src'],
+    install_requires=['ttkbootstrap', 'matplotlib'],
     python_requires='>=3.11',
-    package_data={'src.lang.images': ['*.png']},
+    #package_data={'src.lang.images': ['*.png']},
     entry_points={
         'console_scripts': [
             'prg = main'

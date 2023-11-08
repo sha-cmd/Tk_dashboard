f2ac257683c216972f4e9fc15d5458559c8cc994
Romain Boyrie <romain@boyrie.email>
2023-11-08 18:16:35 +0100

    Structure
    Add a link to the new class.

--- src/objects/controller/__init__.py
+++ src/objects/controller/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["constant", "Downloader", "logger", "Ticket", "toolbox"]
+__all__ = ["constant", "Downloader", "logger", "Ticket", "toolbox", "Analyse"]
 
 import src.objects.controller.constant
 import src.objects.controller.Downloader
@@ -15,3 +15,4 @@ from src.objects.controller.constant import config_exist
 from src.objects.controller.constant import set_config
 from src.objects.controller.Downloader import Downloader
 from src.objects.controller.Ticket import Ticket
+from src.objects.controller.Analyse import Analyse
